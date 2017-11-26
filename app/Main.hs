5d3d8b266ec96be23c3ca392cdb0b1c55f292799
Damian Gaweda <damian.gaweda@gmail.com>
2017-11-26 13:01:25 +0100

    Added 4 extra lines of conf

--- app/Main.hs
+++ app/Main.hs
@@ -13,13 +13,17 @@ data AppCfg
   , acDbUser              :: T.Text
   , acDbPass              :: T.Text
   , acSessionCookie       :: T.Text
-  , acPort                :: Int
+  , acPort                :: T.Text
   , acName                :: T.Text
   , acDesc                :: T.Text
   , acEmailName           :: T.Text
   , acEmailAddr           :: T.Text
   , acMailGunApiKey       :: T.Text
   , acMailGunDomain       :: T.Text
+  , extraConf1            :: T.Text
+  , extraConf2            :: T.Text
+  , extraConf3            :: T.Text
+  , extraConf4            :: T.Text
   } deriving Show
 
 parseConfig :: FilePath -> IO AppCfg
@@ -38,8 +42,13 @@ parseConfig cfgFile =
        emailAddr <- C.require cfg "emailAddr"
        mailGunApiKey <- C.require cfg "mailGunApiKey"
        mailGunDomain <- C.require cfg "mailGunDomain"
+       extraConf1 <- C.require cfg "extraConf1"
+       extraConf2 <- C.require cfg "extraConf2"
+       extraConf3 <- C.require cfg "extraConf3"
+       extraConf4 <- C.require cfg "extraConf4"
        return (AppCfg baseUrl dbName dbHost dbUser dbPass sessCookie port
-                      name desc emailName emailAddr mailGunApiKey mailGunDomain)
+                      name desc emailName emailAddr mailGunApiKey mailGunDomain
+                      extraConf1 extraConf2 extraConf3 extraConf4)
 
 main :: IO ()
 main = do
